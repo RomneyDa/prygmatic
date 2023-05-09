33a8dbad707fe62d369d6a75cb17e3dd7a619c5b
Dallin Romney <dallinromney@gmail.com>
2023-05-08 23:12:50 -0600

    simple git endpoint, .gitignore and vercel.json for redirects

--- api/index.py
+++ api/index.py
@@ -1,10 +1,16 @@
 from http.server import BaseHTTPRequestHandler
- 
+import re
+import json
+
 class handler(BaseHTTPRequestHandler):
- 
+
     def do_GET(self):
+        print(f"Path: {self.path}")
+        text = "Hello, world!"
+        if re.search('/api/example/*', self.path):
+            text = json.dumps({"hello": "world"})
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(text.encode('utf-8'))
         return
